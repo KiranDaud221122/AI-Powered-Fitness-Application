fcc6b522d103dc6a4e0ec36f360c6e3aeeb7a7e2
Your Name <your@email.com>
2026-01-01 21:19:18 +0530

    adding test cases

--- userservice/userservice/src/main/java/com/fitness/userservice/controller/UserController.java
+++ userservice/userservice/src/main/java/com/fitness/userservice/controller/UserController.java
@@ -15,16 +15,16 @@ public class UserController {
 
     private UserService userService;
 
-    @GetMapping("/{userId}")
-    public ResponseEntity<UserResponse> getUserProfile(@PathVariable String userId){
-        return ResponseEntity.ok(userService.getUserProfile(userId));
-    }
-
     @PostMapping("/register")
     public ResponseEntity<UserResponse> register(@Valid @RequestBody RegisterRequest request){
         return ResponseEntity.ok(userService.register(request));
     }
 
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserResponse> getUserProfile(@PathVariable String userId){
+        return ResponseEntity.ok(userService.getUserProfile(userId));
+    }
 
     @GetMapping("/{userId}/validate")
     public ResponseEntity<Boolean> validateUser(@PathVariable String userId){
---
+++ userservice/userservice/src/test/java/com/fitness/userservice/repository/UserRepositoryTest.java
@@ -0,0 +1,20 @@
+package com.fitness.userservice.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserRepositoryTest {
+
+    @Test
+    void existsByEmail() {
+    }
+
+    @Test
+    void existsByKeycloakId() {
+    }
+
+    @Test
+    void findByEmail() {
+    }
+}
